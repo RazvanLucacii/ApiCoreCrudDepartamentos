c2a91a38dafc628f10fc80dd7f6c5a53e5baa202
RazvanLucacii <rlucaci48@gmail.com>
2024-04-09 09:36:18 +0200

    commit

--- ApiCoreCrudDepartamentos/Program.cs
+++ ApiCoreCrudDepartamentos/Program.cs
@@ -1,6 +1,7 @@
 using ApiCoreCrudDepartamentos.Data;
 using ApiCoreCrudDepartamentos.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Api Departamentos",
+        Description = "Api crud de Departamentos"
+    });
+});
 
 string connectionString = builder.Configuration.GetConnectionString("SqlAzure");
 builder.Services.AddTransient<RepositoryDepartamentos>();
@@ -17,11 +25,16 @@ builder.Services.AddDbContext<DepartamentoContext>(options => options.UseSqlServ
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Api Crud de Departamentos v1");
+    options.RoutePrefix = "";
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
